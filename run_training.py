64ce925ad986c83ca04349e3d1384e28a6c5cdd2
Yael CLUSMAN <yael.clusman@yrnet.com>
2026-06-09 17:10:30 +0200

    Fix: execute wine instead of wine64 to avoid FileNotFoundError on VM

--- run_training.py
+++ run_training.py
@@ -364,7 +364,7 @@ def main():
         if sys.platform == "linux":
             env["DISPLAY"] = xvfb_display
             cmd_launch = [
-                "wine64",
+                "wine",
                 str(balatro_exe)
             ]
         else:
